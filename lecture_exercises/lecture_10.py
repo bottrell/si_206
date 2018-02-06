5e310406d514efa62d93d3b23a99959dedb925eb
Jordan Bottrell <bottrell@umich.edu>
2018-02-06 16:35:49 -0500

    lec 10

---
+++ lecture_exercises/lecture_10.py
@@ -0,0 +1,13 @@
+#types of authorization
+#A. None
+#B. API Key
+#C. Basic Authentication: username/ password
+#D. HTTP Digest Authentication: special one time code
+#E. OAuth: doesn't use passwords
+	#OAuth1 vs OAuth2
+	#pretty much the same for the scope of this class
+	#OAuth2 allows for refresh tokens
+
+#https://github.com/bottrell/lecture_10
+
+
