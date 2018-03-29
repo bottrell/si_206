2b65f27b9242052363b7bbf5f71b147fd1213ad4
Jordan Bottrell <bottrell@umich.edu>
2018-03-29 19:36:09 -0400

    falsk

--- lecture_exercises/lec22/helloflask.py
+++ lecture_exercises/lec22/helloflask.py
@@ -3,23 +3,40 @@ app = Flask(__name__)
     
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>'
+	html = '<h1>Jordan Bottrell</h1>'
+	html += '<p> Hi my name is Jordan and I like to code</p>'
+	html +='''<table>
+	<tr> 
+		<th>Stuff I know</th>
+	</tr>
+	<tr> 
+		<td> How to eat</td> 
+	</tr>
+	<tr> 
+		<td> How to read</td>
+	</tr>
+	<tr>
+		<td> How to type quickly </td>
+	</tr>
+	</table>'''
+
+	return html
 
 @app.route('/about')
 def about():    
     html = '''        
-      <h1>About this Site </h1>        
-      <p> This is my first ever Flask website! </p>      
+      <h1>About Jordan </h1>        
+      <p> Jordan knows more stuff than the average toddler </p>      
       <a href='/'> Go back home </a>    
       '''    
     return html
 
 ctr = 0
-@app.route('/counter')
+@app.route('/')
 def counter():    
   global ctr    
   ctr += 1    
-  return '<h3>' + str(ctr) + '</h3>'
+  return '<p> This site has been visited: </p> <h3>' + str(ctr) + '</h3>'
 
 if __name__ == '__main__':
 	print('starting Flask app', app.name)
