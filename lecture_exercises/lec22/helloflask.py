7229aff0cd0584550fe32b192344a83fe7c5cf84
Jordan Bottrell <bottrell@umich.edu>
2018-03-29 09:42:41 -0400

    flask

---
+++ lecture_exercises/lec22/helloflask.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask(__name__)
+    
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+
+@app.route('/about')
+def about():    
+    html = '''        
+      <h1>About this Site </h1>        
+      <p> This is my first ever Flask website! </p>      
+      <a href='/'> Go back home </a>    
+      '''    
+    return html
+
+ctr = 0
+@app.route('/counter')
+def counter():    
+  global ctr    
+  ctr += 1    
+  return '<h3>' + str(ctr) + '</h3>'
+
+if __name__ == '__main__':
+	print('starting Flask app', app.name)
+	app.run(debug=True)
--- lecture_exercises/lec_21.py
+++ lecture_exercises/lec_21.py
@@ -1,6 +1,28 @@
-statement = '''SELECT Employee.Id FROM Employee WHERE Employee.FirstName = 'Anne' AND Employee.LastName = 'Dodsworth'
+statement = '''
+INSERT INTO EmployeeTerritory (Id, EmployeeId, TerritoryId)
+SELECT Employee.Id || '/' || Territory.Id, Employee.Id, Territory.Id
+FROM Employee, Territory
+WHERE Employee.FirstName = 'Anne' 
+  AND Employee.LastName = 'Dodsworth'
+  AND Territory.TerritoryDescription = 'Ann Arbor'
+  '''
 
-SELECT Territory.Id
-FROM Territory
-WHERE Territory.TerritoryDescription = 'Ann Arbor'
-'''
+
+def add_territory_slick(zip_code, desc, region_name):
+    conn = sqlite3.connect('Northwind_small.sqlite')
+    cur = conn.cursor()
+    insert = '''
+        INSERT INTO Territory (Id, TerritoryDescription, RegionId)
+        SELECT ?, ?, Region.Id
+        FROM Region
+        WHERE Region.RegionDescription=?
+    '''
+    params = (zip_code, desc, region_name, )
+    cur.execute(insert, params)
+
+    print(cur.rowcount, 'rows inserted')
+    conn.commit()
+    conn.close()
+
+add_territory_slick('48104', 'Ann Arbor', 'Northern')
+
